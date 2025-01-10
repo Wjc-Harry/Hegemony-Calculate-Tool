f1d6839ed41e7307a09cfdcce409855fea59bb5c
Wjc-Harry <wangjingcheng101@gmail.com>
2025-01-09 23:50:42 -0800

    Add files via upload

--- Calculating tool.js
+++ Calculating tool.js
@@ -95,3 +95,170 @@ function taxCalculator(option1,option2,population){
     }
 }
 
+class welfareStateHealth{
+constructor(healthPolicy,taxMultiplier4){
+    this.healthPolicy=healthPolicy;
+    this.taxMultiplier4=taxMultiplier4
+}
+}
+const A4=new welfareStateHealth("4A",2)
+const B4=new welfareStateHealth("4B",1)
+const C4=new welfareStateHealth("4C",0)
+
+class welfareStateEdu{
+    constructor(eduPolicy,taxMultiplier5){
+        this.eduPolicy=eduPolicy;
+        this.taxMultiplier5=taxMultiplier5
+    }
+    }
+const A5=new welfareStateEdu("5A",2)
+const B5=new welfareStateEdu("5B",1)
+const C5=new welfareStateEdu("5C",0)
+
+class tax{
+    constructor(taxPolicy,basicMultiplier,welfareMultiplier){
+        this.taxPolicy=taxPolicy;
+        this.basicMultiplier=basicMultiplier;
+        this.welfareMultiplier=welfareMultiplier
+    }
+}
+
+const A3=new tax("3A",3,2)
+const B3=new tax("3B",2,1)
+const C3=new tax("3C",1,0)
+
+function taxMultiplierCal(policy3,policy4,policy5){
+    if(policy3==="3A"){
+        if(policy4==="4A"){
+            if(policy5==="5A"){
+                var taxMultiplier=A3.basicMultiplier+A4.taxMultiplier4*A3.welfareMultiplier+A5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5B"){
+                var taxMultiplier=A3.basicMultiplier+A4.taxMultiplier4*A3.welfareMultiplier+B5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+        }else if(policy5==="5C"){
+            var taxMultiplier=A3.basicMultiplier+A4.taxMultiplier4*A3.welfareMultiplier+C5.taxMultiplier5*A3.welfareMultiplier
+            console.log(taxMultiplier)
+            return taxMultiplier
+        }
+        }else if(policy4==="4B"){
+            if(policy5==="5A"){
+                var taxMultiplier=A3.basicMultiplier+B4.taxMultiplier4*A3.welfareMultiplier+A5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5B"){
+                var taxMultiplier=A3.basicMultiplier+B4.taxMultiplier4*A3.welfareMultiplier+B5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5C"){
+                var taxMultiplier=A3.basicMultiplier+B4.taxMultiplier4*A3.welfareMultiplier+C5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+            return taxMultiplier
+            }
+        }else if(policy4==="4C"){
+            if(policy5==="5A"){
+                var taxMultiplier=A3.basicMultiplier+C4.taxMultiplier4*A3.welfareMultiplier+A5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5B"){
+                var taxMultiplier=A3.basicMultiplier+C4.taxMultiplier4*A3.welfareMultiplier+B5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5C"){
+                var taxMultiplier=A3.basicMultiplier+C4.taxMultiplier4*A3.welfareMultiplier+C5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+            return taxMultiplier
+            }
+        }
+    }else if(policy3==="3B"){
+        if(policy4==="4A"){
+            if(policy5==="5A"){
+                var taxMultiplier=B3.basicMultiplier+A4.taxMultiplier4*A3.welfareMultiplier+A5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5B"){
+                var taxMultiplier=B3.basicMultiplier+A4.taxMultiplier4*A3.welfareMultiplier+B5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+        }else if(policy5==="5C"){
+                var taxMultiplier=B3.basicMultiplier+A4.taxMultiplier4*A3.welfareMultiplier+C5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }
+        }else if(policy4==="4B"){
+            if(policy5==="5A"){
+                var taxMultiplier=B3.basicMultiplier+B4.taxMultiplier4*A3.welfareMultiplier+A5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5B"){
+                var taxMultiplier=B3.basicMultiplier+B4.taxMultiplier4*A3.welfareMultiplier+B5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5C"){
+                var taxMultiplier=B3.basicMultiplier+B4.taxMultiplier4*A3.welfareMultiplier+C5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }
+        }else if(policy4==="4C"){
+            if(policy5==="5A"){
+                var taxMultiplier=B3.basicMultiplier+C4.taxMultiplier4*A3.welfareMultiplier+A5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5B"){
+                var taxMultiplier=B3.basicMultiplier+C4.taxMultiplier4*A3.welfareMultiplier+B5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5C"){
+                var taxMultiplier=B3.basicMultiplier+C4.taxMultiplier4*A3.welfareMultiplier+C5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }
+        }
+    }else if(policy3==="3C"){
+        if(policy4==="4A"){
+            if(policy5==="5A"){
+                var taxMultiplier=C3.basicMultiplier+A4.taxMultiplier4*A3.welfareMultiplier+A5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5B"){
+                var taxMultiplier=C3.basicMultiplier+A4.taxMultiplier4*A3.welfareMultiplier+B5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+        }else if(policy5==="5C"){
+                var taxMultiplier=C3.basicMultiplier+A4.taxMultiplier4*A3.welfareMultiplier+C5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+        }
+        }else if(policy4==="4B"){
+            if(policy5==="5A"){
+                var taxMultiplier=C3.basicMultiplier+B4.taxMultiplier4*A3.welfareMultiplier+A5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5B"){
+                var taxMultiplier=C3.basicMultiplier+B4.taxMultiplier4*A3.welfareMultiplier+B5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5C"){
+                var taxMultiplier=C3.basicMultiplier+B4.taxMultiplier4*A3.welfareMultiplier+C5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }
+        }else if(policy4==="4C"){
+            if(policy5==="5A"){
+                var taxMultiplier=C3.basicMultiplier+C4.taxMultiplier4*A3.welfareMultiplier+A5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5B"){
+                var taxMultiplier=C3.basicMultiplier+C4.taxMultiplier4*A3.welfareMultiplier+B5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }else if(policy5==="5C"){
+                var taxMultiplier=C3.basicMultiplier+C4.taxMultiplier4*A3.welfareMultiplier+C5.taxMultiplier5*A3.welfareMultiplier
+                console.log(taxMultiplier)
+                return taxMultiplier
+            }
+        }
+    }
+}
